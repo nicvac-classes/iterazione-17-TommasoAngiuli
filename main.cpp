54e1d07d1ef195486762d6a2d28fb404669ac79c
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2024-10-30 18:18:00 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 using namespace std;
+int main() 
+{
+    int n, i, max, min, negativi, somma;
+    double media;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    cout << "inserisci un numero" << endl;
+    cin >> n;
+    max = n;
+    min = n;
+    i = 0;
+    somma = 0;
+    negativi = 0;
+    while (n != 0) {
+        if (n > 0) {
+            i = i + 1;
+            somma = somma + n;
+        } else {
+            negativi = negativi + 1;
+        }
+        if (min < n) {
+        } else {
+            min = n;
+        }
+        if (max < n) {
+            max = n;
+        }
+        cout << "inserisci un numero" << endl;
+        cin >> n;
+    }
+    media = (double) somma / i;
+    cout << "il valore massimo è " << max << ", il valore minimo è " << min << ", la media dei valori positivi è " << media << ", i valori negativi inseriti sono " << negativi << endl;
+    return 0;
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
